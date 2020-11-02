231a2f0e04cc4f2457b50872c9727c8397c2ab42
Empoliam <liamjwright@btinternet.com>
2020-11-02 23:26:18 +0000

    final version

--- cw14.m
+++ cw14.m
@@ -84,7 +84,7 @@ while i <= length(orbList(1,:)) && ~isnan(orbList(1,i))
     
     %Solve for one full orbit
     [~,~,orb] = MyIVP(@(t,x) rhs(x,orbList(3,i)),[xStar,orbList(2,i)],[0,orbList(1,i)],N,'dp45');
-   
+    
     %Calculate min and max
     rangeI(2,i) = min(orb(1,:));
     rangeI(3,i) = max(orb(1,:));
@@ -104,15 +104,6 @@ hold on
 scatter(ylist(3,:),ylist(1,:),15,stab,'filled')
 gscatter(rangeI(1,:),rangeI(2,:),orbStab,'gr','__',[5,5],false)
 gscatter(rangeI(1,:),rangeI(3,:),orbStab,'gr','++',[5,5],false)
-
-%Legend
-leg = zeros(5, 1);
-leg(1) = plot(NaN,NaN,'or','MarkerFaceColor','r');
-leg(2) = plot(NaN,NaN,'ob','MarkerFaceColor','b');
-leg(3) = plot(NaN,NaN,'og','MarkerFaceColor','g');
-leg(4) = plot(NaN,NaN,'k+');
-leg(5) = plot(NaN,NaN,'k_');
-legend(leg, 'Unstable','Saddle','Stable','Orbit Max','Orbit Min','Location','northwest');
 
 xlabel("beta")
 ylabel("I")
@@ -155,7 +146,7 @@ while i <= 10
     
     %Plot orbit
     subplot(5,2,i)
-        
+    
     %Colour by stability
     if(orbStab(j) == 1)
         orbStyle = 'g';
@@ -215,14 +206,14 @@ dres = @(x) MyJacobian(res,x,jacobianAccuracy*1e-6);
 x0 = [foldApprox;eVec(:,2)];
 
 %Converge to fold point
-sol = MySolve(res,x0,dres,'tol',5e-3);
+solFold = MySolve(res,x0,dres,'tol',5e-3);
 
 %Print result
-disp(["Fold at beta = ", num2str(sol(3))])
+disp(["Fold at beta = ", num2str(solFold(3))])
 
 figure(2)
 hold on
-plot(sol(3),sol(1),'o','MarkerFaceColor',[1,0,1],'MarkerEdgeColor',[1,0,1],'MarkerSize',4)
+plot(solFold(3),solFold(1),'o','MarkerFaceColor',[1,0,1],'MarkerEdgeColor',[1,0,1],'MarkerSize',4)
 
 %Legend
 leg = zeros(2, 1);
@@ -233,18 +224,46 @@ legend(leg, 'Unstable','Stable','Fold','Location','northeast');
 
 hold off
 
+%Find min and max of fold orbit
+
+%Solve for one full orbit
+[~,~,orbFold] = MyIVP(@(t,x) rhs(x,solFold(3)),[xStar,solFold(2)],[0,solFold(1)],N,'dp45');
+
+%Calculate min and max
+rangeIFold = [0;0];
+rangeIFold(1) = min(orbFold(1,:));
+rangeIFold(2) = max(orbFold(1,:));
+
+%Plot on bifurcation diagram
+figure(1)
+hold on
+
+plot(solFold(3),rangeIFold(1),'_','Color',[1,0,1])
+plot(solFold(3),rangeIFold(2),'+','Color',[1,0,1])
+
+%Legend
+leg = zeros(5, 1);
+leg(1) = plot(NaN,NaN,'or','MarkerFaceColor','r');
+leg(2) = plot(NaN,NaN,'ob','MarkerFaceColor','b');
+leg(3) = plot(NaN,NaN,'og','MarkerFaceColor','g');
+leg(4) = plot(NaN,NaN,'k+');
+leg(5) = plot(NaN,NaN,'k_');
+legend(leg, 'Unstable','Saddle','Stable','Orbit Max','Orbit Min','Location','northwest');
+
+hold off
+
 %%Track fold point - INCOMPLETE
-% 
+%
 % M2 = @(t1,x0,p) MyIVP(@(t,x) rhs2P(x,p(1),(2)),x0,[0,t1],N,'dp45');
-% 
+%
 % g = @(x) M2(x(1),[xStar;x(2)],x(3:4)) - [xStar;x(2)];
 % JM2 = @(x) MyJacobian(@(y) M2(x(1),y,x(3:4)), [xStar;x(2)], jacobianAccuracy);
-% 
+%
 % res2 = @(x) [g(x(1:4)); JM2(x(1:4)) * x(5:6); x(5:6)' * x(5:6) - 1];
 % dres2 = @(x) MyJacobian(res,x,jacobianAccuracy);
-% 
+%
 % tan1 = [0;0;0;1;0;0];
-% 
+%
 % [eVec,~] = eigs(JM2([foldApprox;gamma0]));
 % fInit0 = [foldApprox;gamma0;eVec(:,2)];
 % trackFoldList(:,:,1) = MyTrackCurve(res2,fInit0,tan1,'sMax',0.01,'nMax',10,'correctGuess',false);
